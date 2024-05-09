e001142b98a20407855930be82f596ab98d122ea
SmitBorsadiya <borsadiya5525smit@gmail.com>
2024-05-09 08:38:11 +0530

    action setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { addToCart } from './redux/action';
+import {useDispatch} from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+  const data = {
+    phone:"Oppo",
+    price:15000,
+    color:"blue"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>dispatch(addToCart(data))}>Add To Cart</button>
     </div>
   );
 }
