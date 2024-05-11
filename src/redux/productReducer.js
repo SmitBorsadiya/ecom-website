4bef4c0271a6d5515ecf348349107abbaee7656f
SmitBorsadiya <borsadiya5525smit@gmail.com>
2024-05-11 14:57:57 +0530

    saga configuration

--- src/redux/productReducer.js
+++ src/redux/productReducer.js
@@ -1,10 +1,10 @@
-import { PRODUCT_LIST } from "./constant";
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
 
 export const productList = (data = [], action) => {
   switch (action.type) {
-    case PRODUCT_LIST:
-      console.warn("PRODUCT_LIST condition ", action);
-      return [action.data];
+    case SET_PRODUCT_LIST:
+      // console.warn("SET_PRODUCT_LIST condition ", action);
+      return [...action.data];
     default:
       return data;
   }
